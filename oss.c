5fd6e30d027b82dfabd7775963d0e395d630833e
Alexander Kane <o2-kane@hoare7.cs.umsl.edu>
2018-04-19 21:38:49 -0500

    4/19 initial executable check in

---
+++ oss.c
@@ -0,0 +1,28 @@
+// oss.c
+// CS 4760 Project 6 
+// Alex Kane 4/19/2018
+// Master executable code - contains most of application logic
+#include<unistd.h>
+#include<math.h>
+#include<signal.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/ipc.h>
+#include<sys/sem.h>
+#include<sys/shm.h>
+#include<sys/msg.h>
+#include<time.h>
+#include"ErrorLogging.h"
+#include"IPCUtilities.h"
+#include"QueueUtilities.h"
+#include"PeriodicTimer.h"
+#include"ProcessControlBlock.h"
+#include"ProcessUtilities.h"
+#include"StringUtilities.h"
+
+int main(int argc, char** argv)
+{
+	return 0;
+}
